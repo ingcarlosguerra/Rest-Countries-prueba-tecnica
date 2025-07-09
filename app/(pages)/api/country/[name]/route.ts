8d388085abad04ba8dc0757aeea73d8c94cb3cba
ingcarlosguerra <est.carlos.guerra@unimilitar.edu.co>
2025-07-09 17:33:45 -0500

    fixing routes

--- app/(pages)/api/country/[name]/route.ts
+++ app/(pages)/api/country/[name]/route.ts
@@ -1,33 +1,30 @@
-
+// app/(pages)/api/country/[name]/route.ts
 import axios from 'axios';
 import { NextResponse } from 'next/server';
 
 export async function GET(
   _req: Request,
-  { params }: { params: { name: string } }
+  { params }: { params: Promise<{ name: string }> }
 ) {
-  const { name } = params;                 
+
+  const { name } = await params;
 
   try {
     const { data } = await axios.get(
-      `${process.env.RESTCOUNTRIES_BASE_URL}/name//${encodeURIComponent(name)}`,
+      `${process.env.RESTCOUNTRIES_BASE_URL}/name/${encodeURIComponent(name)}`,
       {
-
         params: {
           fields:
             'flags,flag,name,borders,population,region,capital,subregion,currencies,languages',
-
         },
       }
     );
 
     const country = Array.isArray(data) ? data[0] : data;
-    //console.log(country);
     return NextResponse.json(country);
   } catch (error: any) {
     const status = error.response?.status ?? 500;
     const message = error.response?.data ?? error.message;
-    console.error('Error al obtener país:', message);
     return NextResponse.json({ error: message }, { status });
   }
 }
